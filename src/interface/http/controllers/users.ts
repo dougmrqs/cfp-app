ab4c4d32afc42d96a7281cad1484ef5024494c4e
Douglas Marques <douglas.bmarques@hotmail.com>
2024-10-15 13:02:28 -0300

    add handler type

--- src/interface/http/controllers/users.ts
+++ src/interface/http/controllers/users.ts
@@ -1,13 +1,19 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { createUser } from '../../../use-cases/users/create'
 
-export async function create(request: FastifyRequest, reply: FastifyReply) {
+type CreateBody = {
+  email: string,
+  username: string,
+  birthDate: string
+}
+
+export async function create(request: FastifyRequest<{ Body: CreateBody }>, reply: FastifyReply) {
   const { body } = request
 
   const user = await createUser({
     email: body.email,
     username: body.username,
-    birthDate: body.birthDate
+    birthDate: new Date(body.birthDate)
   })
 
   reply.status(201).send(user)
